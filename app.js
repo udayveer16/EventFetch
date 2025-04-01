d82a940aaa78b9234d1fae9e6e1d0aeb3c6be02d
udayveer16 <udayveer1609@gmail.com>
2025-04-01 15:24:08 +0530

    sqrtPriceX96 included

--- app.js
+++ app.js
@@ -14,21 +14,23 @@ async function getCurrentBlockNumber() {
 async function fetchEvents() {
     let contract = new ethers.Contract(USDC_ETH_POOL_ADDRESS, ABI, provider);
     console.log("Listening to events...");
-    console.log("Type \t \t Sender \t Recipient \t Amount USDC \t Amount ETH \t Price");
-    console.log("---------------------------------------------------------------------------------------------");
+    console.log("Type \t \t Sender \t Recipient \t Amount USDC \t Amount ETH \t Price \t \t \t Price in USDC");
+    console.log("-----------------------------------------------------------------------------------------------------------------");
     
     contract.on("Swap", (sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick) => {
         
         let usdcAmount = Number(ethers.formatUnits(amount0, 6)).toFixed(4); // USDC (6 decimals)
         let ethAmount = Number(ethers.formatUnits(amount1, 18)).toFixed(4); // ETH (18 decimals)
         let price = usdcAmount / ethAmount;
+        const Q96 = 2 ** 96;
+        let ethPriceInUSDC = (Number(sqrtPriceX96) / Q96) ** 2;
 
         const formatAddress = (addr) => `${addr.slice(0, 6)}...${addr.slice(-4)}`;
         
         if (amount0 > 0) {
-            console.log("Buy ETH \t", formatAddress(sender) ,"\t", formatAddress(recipient), "\t", usdcAmount, "\t", -(ethAmount), "\t$", -(price));
+            console.log("Buy ETH \t", formatAddress(sender) ,"\t", formatAddress(recipient), "\t", usdcAmount, "\t", -(ethAmount), "\t$", -(price), "\t$", ethPriceInUSDC);
         } else {
-            console.log("Sell ETH\t", formatAddress(sender), "\t", formatAddress(recipient), "\t", -(usdcAmount), "\t", ethAmount ,"\t$", -(price));
+            console.log("Sell ETH\t", formatAddress(sender), "\t", formatAddress(recipient), "\t", -(usdcAmount), "\t", ethAmount ,"\t$", -(price), "\t$", ethPriceInUSDC);
         }
     });
 }
